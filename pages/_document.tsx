c940a7df1795bc034e0bee26818f0727f3124001
João Koritar <jakoritarleite@gmail.com>
2022-05-23 11:02:10 -0300

    feat: create next _document page so it loads fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { getCssText } from '../stitches.config';
+
+export default function Document() {
+  return (
+    <Html lang="en-US">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta content="João Koritar" name="author" />
+        <meta property="og:type" content="website" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta name="theme-color" content="#08070b" />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
